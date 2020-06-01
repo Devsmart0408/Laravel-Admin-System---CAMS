21be4b6aa292966e3a663b73804eee8c8e2146ac
webdevsmart <webdevsmart@hotmail.com>
2020-06-01 21:31:25 +0300

    datatable

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // datatable
+        Yajra\Datatables\DatatablesServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +228,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        
+        'Datatables' => Yajra\Datatables\Facades\Datatables::class,
 
     ],
 
